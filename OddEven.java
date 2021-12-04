e1c61d04ffe583dd4a010914500a949821ce68e9
Ahan <82447217+ahan-stark@users.noreply.github.com>
2021-12-04 18:09:26 +0530

    Update OddEven.java
    Changed the conditional statements

--- OddEven.java
+++ OddEven.java
@@ -2,10 +2,7 @@ public class OddEven {
     public static void main(String[] args) {
         double num;
         num=(Math.floor(Math.random()*10));            //to generate a random input
-        if(num==0){
-            System.out.print(num+" ,it's not an odd/even");
-        }
-        else if(num%2==0)
+        if(num%2==0)
         {
             System.out.print(num+" is an even number");
         }
